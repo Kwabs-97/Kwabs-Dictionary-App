ea05f04f7536284f0ef0ec597f3281522530e3ae
Kwabs-97 <khobewayne@gmail.com>
2023-03-30 21:01:57 +0000

    fixed audio

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-
+/** @format */
 
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const result = document.getElementById('result')
-const sound = document.getElementById('sound')
-const btn = document.getElementById('search-btn')
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
 
-
-
-btn.addEventListener('click', async() => {
+btn.addEventListener("click", async () => {
+  try {
     let inpWord = document.getElementById("inp-word").value;
-    const res = await axios.get(`${url}${inpWord}`)
+    const res = await axios.get(`${url}${inpWord}`);
+    console.log(res);
     let partOfSpeech = res.data[0].meanings[0].partOfSpeech;
     let phonetic = res.data[0].phonetic;
-    let definition = res.data[0].meanings[0].definitions[0].definition
+    let phonetics = res.data[0].phonetics;
+    const filteredPhonetics = phonetics.filter((a) => a.audio !== "")[0].audio;
+    console.log(filteredPhonetics);
+    sound.setAttribute("src", `${filteredPhonetics}`);
+
+    let definition = res.data[0].meanings[0].definitions[0].definition;
     let example = res.data[0].meanings[0].definitions[0].example || "";
 
-
-
-    let pronounciationArray = res.data[0].phonetics;
-    for (let i in pronounciationArray){
-       
-        sound.setAttribute("src", `${pronounciationUrl}`)
-    }
-    
-      
     result.innerHTML = `
            <div class="word">
                 <h3>${inpWord}</h3>
-                <button onclick = "playSound()" >
+                <button onclick="playSound()" >
                     <i class="fa fa-volume-up" aria-hidden="true"></i>
                 </button>
             </div>
@@ -44,10 +40,11 @@ btn.addEventListener('click', async() => {
                 </p>
             </div>
             `;
-            
-            
-})
+  } catch (e) {
+    console.log(e.message);
+  }
+});
 
 function playSound() {
-    sound.play() || ""
+  sound.play();
 }
